a2291a2878dc35c621fcb5a4a074cff5d7f737e6
JABASTIN <itsmejabastin@gmail.com>
2024-04-23 18:04:47 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const background = document.getElementById("background");
+const password = document.getElementById("password");
+
+password.addEventListener("input", (e) => {
+  const value = e.target.value;
+  const length = value.length;
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});
